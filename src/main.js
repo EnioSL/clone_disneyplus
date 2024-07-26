d24d6508dfe26d126fcdedb785f86fbf87f4b6ef
Enio Silveira Leite <eniosleite@yahoo.com.br>
2024-07-26 13:57:56 -0300

    ciando a seção de planos

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
 
+
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
-            const abaAlvo = botao.target.dataset.tabButton
+            const abaAlvo = botao.target.dataset.tabButton;
             const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
-            escondeToasAbas();
+            escondeTodasAbas();
             aba.classList.add('shows__list--is-active');
+            removeBotaoAtivo();
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
@@ -20,7 +22,7 @@ function removeBotaoAtivo() {
     }
 }
 
-function escondeToasAbas() {
+function escondeTodasAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
     for (let i = 0; i < tabsContainer.length; i++) {
